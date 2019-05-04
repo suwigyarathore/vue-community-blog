b365d2a9b1becf1aa5b0485f3b57bc00a609d8a8
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-05-04 16:35:33 +0200

    Signup form set

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>Signup Component</h1>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Signup</h3>
+          <div class="form-group">
+            <input v-model="name" type="text" placeholder="Name" class="form-control">
+          </div>
+          <div class="form-group">
+            <input v-model="email" type="text" placeholder="Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input v-model="password" type="password" placeholder="Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button @click="registerUser()" class="btn form-control btn-success">Signup</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    registerUser() {
+      console.log(this.name, this.email, this.password);
+    }
+  }
+};
+</script>
