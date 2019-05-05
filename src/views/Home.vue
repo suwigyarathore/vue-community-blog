c9d9ed40ad069cca5a097e8fb6d3057f218b8f35
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-05-05 21:05:41 +0200

    Setting up articles in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1 class="text-center my-5">Welcome to the home screen</h1>
+  <div v-if="articles.data" class="row">
+    <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
+      <Article/>
+    </div>
+  </div>
 </template>
+
+
+<script>
+import Axios from "axios";
+import config from "@/config";
+import Article from "@/components/Article.vue";
+export default {
+  components: {
+    Article
+  },
+  mounted() {
+    this.getArticles();
+  },
+  data() {
+    return {
+      articles: {}
+    };
+  },
+  methods: {
+    getArticles() {
+      Axios.get(`${config.apiUrl}/articles`).then(response => {
+        this.articles = response.data.data;
+      });
+    }
+  }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,10 +3,22 @@
     <div class="col-md-6 offset-md-3">
       <div class="card">
         <div class="card-body">
-          <h3 class="text-center my-4">Login</h3>
+          <h3 class="text-center my-4">Signup</h3>
           <div class="form-group">
             <input
-              v-bind:class="{ 'is-invalid': errors.email }"
+              v-bind:class="{ 'is-invalid': errors.name, 'is-valid': !errors.name && this.submitted }"
+              v-model="name"
+              type="text"
+              placeholder="Name"
+              class="form-control"
+            >
+            <div class="errors" v-if="errors.name">
+              <small class="text-danger" :key="error" v-for="error in errors.name">{{ error }}</small>
+            </div>
+          </div>
+          <div class="form-group">
+            <input
+              v-bind:class="{ 'is-invalid': errors.email, 'is-valid': !errors.email && this.submitted }"
               v-model="email"
               type="text"
               placeholder="Email"
@@ -18,7 +30,7 @@
           </div>
           <div class="form-group">
             <input
-              v-bind:class="{ 'is-invalid': errors.password }"
+              v-bind:class="{ 'is-invalid': errors.password, 'is-valid': !errors.password && this.submitted }"
               v-model="password"
               type="password"
               placeholder="Password"
@@ -29,9 +41,13 @@
             </div>
           </div>
           <div class="form-group text-center">
-            <button @click="loginUser()" :disabled="loading" class="btn form-control btn-success">
+            <button
+              @click="registerUser()"
+              :disabled="loading"
+              class="btn form-control btn-success"
+            >
               <i class="fas fa-spin fa-spinner" v-if="loading"></i>
-              {{ loading ? '' : 'Login' }}
+              {{ loading ? '' : 'Signup' }}
             </button>
           </div>
         </div>
@@ -42,6 +58,7 @@
 
 <script>
 import Axios from "axios";
+import config from "@/config";
 export default {
   beforeRouteEnter(to, from, next) {
     if (localStorage.getItem("auth")) {
@@ -51,36 +68,36 @@ export default {
   },
   data() {
     return {
+      name: "",
       email: "",
       password: "",
       errors: {},
+      submitted: false,
       loading: false
     };
   },
   methods: {
-    loginUser() {
+    registerUser() {
       this.loading = true;
-      Axios.post("https://react-blog-api.bahdcasts.com/api/auth/login", {
+      Axios.post(`${config.apiUrl}/auth/register`, {
+        name: this.name,
         email: this.email,
         password: this.password
       })
         .then(response => {
           this.loading = false;
-          this.$root.auth = response.data.data;
-          localStorage.setItem("auth", JSON.stringify(response.data.data));
-          this.$noty.success("Successfully logged in.");
+          this.submitted = true;
+          const { data } = response.data;
+          localStorage.setItem("auth", JSON.stringify(data));
+          this.$root.auth = data;
+          this.$noty.success("Successfully registered.");
           this.$router.push("/");
         })
         .catch(({ response }) => {
+          this.$noty.error("Oops ! Something went wrong.");
           this.loading = false;
-          this.$noty.error("Oops ! something went wrong.");
-          if (response.status === 401) {
-            this.errors = {
-              email: ["These credentials do not match our records."]
-            };
-          } else {
-            this.errors = response.data;
-          }
+          this.submitted = true;
+          this.errors = response.data;
         });
     }
   }
